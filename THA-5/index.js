c3af14106b6e77b4bcc1d0d8354f4d88c012b842
Palaksharma-dotcom <70392674+Palaksharma-dotcom@users.noreply.github.com>
2021-09-08 23:38:12 +0530

    Add files via upload

---
+++ THA-5/index.js
@@ -0,0 +1,7 @@
+// var fs= require("fs");
+// fs.mkdirSync("day_5");
+// fs.writeFileSync("day_5/hello.txt","hello");
+var Test=require("./require_test");
+
+var obj=new Test();
+console.log(obj.print());
---
+++ THA-5/require_test.js
@@ -0,0 +1,13 @@
+class Test{
+    print()
+    {
+        console.log("inside requie test");
+    }
+}
+class Test2{
+    print(){
+        console.log("reserved content");
+    }
+}
+module.exports=Test;
+
